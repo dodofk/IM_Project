4fa415c0a0276953e10c420eb9585df261ce1a43
dodofk <ricky.16811@gmail.com>
2022-03-16 22:38:22 +0800

    save

--- src/models/basic_module.py
+++ src/models/basic_module.py
@@ -11,15 +11,6 @@ from torchmetrics.classification.accuracy import Accuracy
 
 class BasicLitModule(LightningModule):
     """
-    Example of LightningModule for MNIST classification.
-
-    A LightningModule organizes your PyTorch code into 5 sections:
-        - Computations (init).
-        - Train loop (training_step)
-        - Validation loop (validation_step)
-        - Test loop (test_step)
-        - Optimizers (configure_optimizers)
-
     Read the docs:
         https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
     """
