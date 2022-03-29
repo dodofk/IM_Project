d6d4f743bd95e73129a7379869ba5721daa1be36
dodofk <ricky.16811@gmail.com>
2022-03-29 15:01:23 +0800

    set up script to download data from google drive

---
+++ scripts/download_heichole.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install gdown --upgrade
+
+gdown https://drive.google.com/uc?id=1fjpD36RdVF4pnCMLmPP6ZB8cu6EkhbBt
+
+mv HeiChole.zip data/HeiChole_data
+
+cd data/HeiChole_data && unzip HeiChole.zip
