39927ea502191523bca91da4e4d8e12bacd82abc
dodofk <ricky.16811@gmail.com>
2022-03-30 08:17:35 +0800

    set up using simple resnet to do image classification

--- src/models/basic_module.py
+++ src/models/basic_module.py
@@ -1,20 +1,18 @@
 from typing import Any, List, Dict
-from omegaconf import OmegaConf
-from webbrowser import get
-
+from omegaconf import DictConfig
 import timm
 import torch
 import torch.nn as nn
 from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric
-from torchmetrics.classification.accuracy import Accuracy
+from torchmetrics import F1Score
 
 
 class BasicLitModule(LightningModule):
     def __init__(
         self,
-        temporal_model: torch.nn.Module,
-        mlp: object,
+        temporal_cfg: DictConfig,
+        mlp: DictConfig,
         optim: str = "Adam",
         lr: float = 0.001,
         weight_decay: float = 0.0005,
@@ -32,56 +30,68 @@ class BasicLitModule(LightningModule):
         # it also ensures init params will be stored in ckpt
         self.save_hyperparameters(logger=False)
 
-        # TODO: set up ResNet and LSTM, the following structure is a easy guide to implement
-        # you could add any model component or write all model to the ./components folder
-        # i think it might be more appropriate to write in components, so that we could use config to
-        # easily change different model
-
-        # DONE TODO: whether use the timm library or torch hub to load pretrained backbone model
-        # should set a use_timm = true or false to determine
         self.feature_extractor = timm.create_model(
             "resnet34",
             pretrained=use_timm,
-            in_chans = 1,
-            # num_classes = ,
+            in_chans=3,
+            num_classes=0,
         )
 
-        # not sure what name to use
-        # DONE TODO: load the LSTM or other rnn based model it could be determine by config files
-        # calling the falling line will error, since u didn't pass config to instantiate
-        # self.temporal_model = instantiate(temporal_model)
+        # todo: not yet support for tcn, or rnn-based model
+        if temporal_cfg.type is not None:
+            self.temporal_model = getattr(nn, temporal_cfg.type)(
+                input_size=self.feature_extractor.num_features,
+                hidden_size=temporal_cfg.hidden_size,
+                num_layers=temporal_cfg.num_layers,
+                bidirectional=temporal_cfg.bidirectional,
+            )
 
-        # DONE TODO: set up the final linear or with some activation functions
         self.mlp = nn.Sequential(
-            nn.Linear(mlp.input_size, 32),
-            nn.BatchNorm1d(32),
+            nn.Linear(
+                self.feature_extractor.num_features * self.temporal_direction(),
+                mlp.hidden_size,
+            ),
+            nn.BatchNorm1d(mlp.hidden_size),
             nn.ReLU(),
-            nn.Linear(32, mlp.output_size),
-            nn.BatchNorm1d(mlp.output_size),
-            nn.ReLU(),
+            nn.Linear(mlp.hidden_size, self.num_class()),
         )
 
-        # DONE TODO: choose the proper loss function, since i remember CE is better
-        # use in one class for one instance ? or maybe i'm wrong
-        self.criterion = torch.nn.CrossEntropyLoss()
+        if task in ["phase"]:
+            self.criterion = torch.nn.CrossEntropyLoss()
+        elif task in ["tool", "action"]:
+            self.criterion = torch.nn.BCEWithLogitsLoss()
 
         # use separate metric instance for train, val and test step
         # to ensure a proper reduction over the epoch
-        self.train_acc = Accuracy()
-        self.val_acc = Accuracy()
-        self.test_acc = Accuracy()
+        self.train_f1 = F1Score()
+        self.val_f1 = F1Score()
 
         # for logging best so far validation accuracy
-        self.val_acc_best = MaxMetric()
+        self.val_f1_best = MaxMetric()
+
+    def num_class(self):
+        task_class = {
+            "tool": 7,
+            "phase": 7,
+            "action": 4,
+        }
+        return task_class.get(self.hparams.task)
+
+    def temporal_direction(self):
+        if (
+            self.hparams.temporal_cfg.type is None
+            or not self.hparams.temporal_cfg.bidirectional
+        ):
+            return 1
+        else:
+            return 2
 
     def forward(self, x):
-        """
-        return a tensor in size B * ? * (4 if task is action else 7)
-        where B is the Batch Size ? according to the model setting
-        """
-        # DONE TODO: finish the foward part
+
         x = self.feature_extractor(x)
-        x, _ = self.hparams.temporal_model(x)
+
+        if self.hparams.temporal_cfg.type is not None:
+            x, _ = self.hparams.temporal_model(x)
         x = self.mlp(x)
 
         return x
@@ -107,19 +117,18 @@ class BasicLitModule(LightningModule):
         # TODO: finish the step part and choose the proper loss function for multi-classification
         # print(batch[self.hparams.task])
         # x, y = batch["image"], batch[self.hparams.task]
-        x, y = batch
-        logits = self.forward(x)
-        loss = self.criterion(logits, y)
-        preds = torch.argmax(logits, dim=1)
-        return loss, preds, y
+        logits = self.forward(batch["image"])
+        loss = self.criterion(logits, batch[self.hparams.task])
+        preds = torch.argmax(logits, dim=-1)
+        return loss, preds, batch[self.hparams.task]
 
     def training_step(self, batch: Any, batch_idx: int):
         loss, preds, targets = self.step(batch)
 
         # log train metrics
-        acc = self.train_acc(preds, targets)
+        f1 = self.train_f1(preds, targets)
         self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/f1", f1, on_step=False, on_epoch=True, prog_bar=True)
 
         # we can return here dict with any tensors
         # and then read it in some callback or in `training_epoch_end()`` below
@@ -134,37 +143,29 @@ class BasicLitModule(LightningModule):
         loss, preds, targets = self.step(batch)
 
         # log val metrics
-        acc = self.val_acc(preds, targets)
+        acc = self.val_f1(preds, targets)
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/f1", acc, on_step=False, on_epoch=True, prog_bar=True)
 
         return {"loss": loss, "preds": preds, "targets": targets}
 
     def validation_epoch_end(self, outputs: List[Any]):
-        acc = self.val_acc.compute()  # get val accuracy from current epoch
-        self.val_acc_best.update(acc)
+        f1 = self.val_f1.compute()  # get val accuracy from current epoch
+        self.val_f1_best.update(f1)
         self.log(
-            "val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True
+            "val/f1_best", self.val_f1_best.compute(), on_epoch=True, prog_bar=True
         )
 
     def test_step(self, batch: Any, batch_idx: int):
-        loss, preds, targets = self.step(batch)
-
-        # log test metrics
-        acc = self.test_acc(preds, targets)
-        self.log("test/loss", loss, on_step=False, on_epoch=True)
-        self.log("test/acc", acc, on_step=False, on_epoch=True)
-
-        return {"loss": loss, "preds": preds, "targets": targets}
+        raise NotImplementedError
 
     def test_epoch_end(self, outputs: List[Any]):
         pass
 
     def on_epoch_end(self):
         # reset metrics at the end of every epoch
-        self.train_acc.reset()
-        self.test_acc.reset()
-        self.val_acc.reset()
+        self.train_f1.reset()
+        self.test_f1.reset()
 
     def configure_optimizers(self):
         """Choose what optimizers and learning-rate schedulers to use in your optimization.
