cc1b5d0792bafdd1e3f3b8d6943c58e5732d834c
dodofk <ricky.16811@gmail.com>
2022-05-11 01:48:14 +0800

    update url

--- scripts/download_cholect45.sh
+++ scripts/download_cholect45.sh
@@ -2,7 +2,7 @@
 
 gdown https://drive.google.com/uc?id=14o5HHzK7kSXQxeoOijiPpp_nHl4yer_t
 #
-unzip CholecT45_resize.zip -O
+#unzip CholecT45_resize.zip -O
 #mv CholecT45_resize.zip data/Cholec
 #
 #cd data/HeiChole_data && unzip HeiChole.zip
