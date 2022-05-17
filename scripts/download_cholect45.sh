5d215a1686f39e076844e98fcf2eb6deb3372ac6
dodofk <ricky.16811@gmail.com>
2022-05-17 21:04:06 +0800

    download cholect45 scsript

--- scripts/download_cholect45.sh
+++ scripts/download_cholect45.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 gdown https://drive.google.com/uc?id=14o5HHzK7kSXQxeoOijiPpp_nHl4yer_t
+
+mv CholecT45_resize.tar.gz data
+
+cd data || return
+
+tar -xvf CholecT45_resize.tar.gz
+
+mv CholecT45_resize CholecT45
 #
 #unzip CholecT45_resize.zip -O
 #mv CholecT45_resize.zip data/Cholec
